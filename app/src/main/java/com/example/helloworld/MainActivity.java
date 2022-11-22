15d24d37f4c8c88fb453a109e273e3c7bfdcd198
Yashodha Lakshana <yashodhalakshana@gmail.com>
2022-11-22 22:18:03 +0530

    Second

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,21 +2,69 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE ="com.example.android.helloTest.extra.MESSAGE";
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editTexSend);
+
+        Button logButton = findViewById(R.id.button);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add your code here
+                onButtonClick();
+            }
+        });
+
+        Button secondButton = findViewById(R.id.button2);
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add your code here
+                onButtonClickActivity();
+            }
+        });
+
+        Button buttonSend = findViewById(R.id.button3);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add your code here
+                onButtonClickActivitySend();
+            }
+        });
     }
 
-    public void onButtonClick(View views){
-        TextView txt = findViewById(R.id.textView);
-        txt.setText("FFD");
-
+    public void onButtonClick(){
+        Uri uri = Uri.parse("https://www.google.lk");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(it);
     }
+
+    public void onButtonClickActivity(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void onButtonClickActivitySend(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
